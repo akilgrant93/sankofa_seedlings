bea65a6de7527c093f3d7b83f39170a2ed7a5c29
Akil Grant <akilgrant@Akils-MacBook-Pro.local>
2025-10-16 13:30:21 -0400

    carousel fix

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -9,10 +9,28 @@ interface Props {
   products: Stripe.Product[];
 }
 
+function shuffleArray<T>(array: T[]): T[] {
+    let currentIndex = array.length, randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 export const Carousel = ({ products }: Props) => {
   const [current, setCurrent] = useState<number>(0);
   const activeProducts = products.filter(product => product.active)
-  products = activeProducts
+  products = shuffleArray(activeProducts)
 
   useEffect(() => {
     const interval = setInterval(() => {
