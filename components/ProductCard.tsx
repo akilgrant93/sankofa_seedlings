388d337070b1644f7d9faddfbaa6117895e515ed
Akil Grant <akil.grant.93@gmail.com>
2025-10-17 01:10:34 -0400

    more changes to products page

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,7 +12,7 @@ const ProductCard = ({ product }: Props) => {
   const price = product.default_price as Stripe.Price;
 
   return (
-    <Link href={`/products/${product.id}`} className="block h-full">
+    <Link href={`/products/${product.id}`} className="block h-full w-[25vw]">
       <Card className="group hover:shadow-2xl shadow transition duration-300 py-0 h-full flex flex-col border-gray-300 gap-0">
         {product.images && product.images[0] && (
           <div className="relative h-60 w-full">
@@ -25,20 +25,20 @@ const ProductCard = ({ product }: Props) => {
           </div>
         )}
         <CardHeader className="px-4 pt-4">
-          <CardTitle className="text-lg font-bold text-gray-800">
+            {price && price.unit_amount && (
+              <p className="text-lg font-semibold text-gray-900">
+                ${(price.unit_amount / 100).toFixed(2)}
+              </p>
+            )}
+          <CardTitle className="text-base font-light text-gray-800">
             {product.name}
           </CardTitle>
         </CardHeader>
-        <CardContent className="p-4 flex-grow flex flex-col justify-between">
-          {product.description && (
+        <CardContent className="p-4 flex-grow flex flex-col justify-end">
+          {/* {product.description && (
             <p className="text-gray-600 text-xs mb-2">{product.description}</p>
-          )}
-          {price && price.unit_amount && (
-            <p className="text-lg font-semibold text-gray-900">
-              ${(price.unit_amount / 100).toFixed(2)}
-            </p>
-          )}
-          <Button className="mt-4 bg-[#285943] text-white">View Details</Button>
+          )} */}
+          <Button className="mt-4 bg-[#285943] text-white cursor-pointer">View Details</Button>
         </CardContent>
       </Card>
     </Link>
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -24,14 +24,17 @@ const ProductList = ({ products }: Props) => {
 
   return (
     <div>
-      <div className="flex justify-end items-center my-4">
+      <div className="my-4 w-[100vw]">
+        {/* sort by name + sort alphabetically + justify between fix for outer div*/}
+        <div className="w-[77.5vw] flex justify-end items-center mx-auto">
         <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search products..."
-          className="w-full max-w-md rounded border placeholder:text-[#77AF9C] text-[#77AF9C] placeholder:text-sm border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full max-w-md rounded border shadow-none hover:shadow-sm transition-shadow duration-300 ease-in-out placeholder:text-[#77AF9C] text-[#77AF9C] placeholder:text-sm border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
+        </div>
       </div>
       <div className="flex flex-col items-center justify-center md:min-h-[35vh]">
       {filteredProduct.length  ? <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
